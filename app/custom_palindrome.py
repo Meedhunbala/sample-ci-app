22e109776ef23cb0ff22315a3e0f043769270f34
Balamurali R <balamurali.r@lifetech.com>
2020-02-24 20:39:37 +0530

    sample application with docker

---
+++ app/custom_palindrome.py
@@ -0,0 +1,39 @@
+import argparse
+import re
+
+IGNORE_NON_ALPHA_CHARACTER = re.compile(r"\W+|_")
+
+
+def isPalindrome(word):
+    """
+       Check the give word is palindrome.
+
+       Args:
+          :param word(str): string input
+       Returns:
+          bool: True|False
+    """
+
+    input_str = IGNORE_NON_ALPHA_CHARACTER.sub("", str(word)).casefold()
+    return input_str == input_str[::-1]
+
+
+def make_cli_parser():
+    cli_parser = argparse.ArgumentParser(add_help=True)
+    cli_parser.add_argument(
+        'word',
+        help="string to verify..")
+    return cli_parser
+
+
+def main(argv=None):
+    cli_parser = make_cli_parser()
+    args = cli_parser.parse_args(argv)
+    print("Given string is a Palindrome? {}".format(isPalindrome(args.word)))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as error:
+        raise error
---
+++ tests/test_custom_palindrome.py
@@ -0,0 +1,55 @@
+from custom_palindrome import isPalindrome, make_cli_parser
+import unittest
+
+
+class TestPalindrome(unittest.TestCase):
+
+    def test_isPalindrome_with_alphabets(self):
+        expected_results = True
+        actual_results = isPalindrome("madam")
+        self.assertEqual(
+            expected_results, actual_results, "alpha-test-passed!!")
+
+    def test_isPalindrome_with_numeric(self):
+        expected_results = True
+        actual_results = isPalindrome(121121)
+        self.assertEqual(
+            expected_results, actual_results, "numeric-test-passed!!")
+
+    def test_isPalindrome_with_alphanumeric(self):
+        expected_results = True
+        actual_results = isPalindrome("001eye100")
+        self.assertEqual(
+            expected_results, actual_results, "alpha-numeric-test-passed!!")
+
+    def test_isPalindrome_with_non_alphanumeric(self):
+        expected_results = [True, True, True]
+        inputs = ["_!MadAm@$", "RACEC@A@R", "RE&p*a(pe_r"]
+        actual_results = [isPalindrome(_) for _ in inputs]
+        self.assertEqual(
+            expected_results,
+            actual_results,
+            "non-alpha-numeric-test-passed!!")
+
+
+class TestArgumentsParsers(unittest.TestCase):
+
+    def setUp(self):
+        self.cli_parser = make_cli_parser()
+
+    def test_arguments(self):
+        args = ['test']
+        args_namespace = self.cli_parser.parse_args(args)
+        expected_arguments = "test"
+
+        self.assertEqual(expected_arguments, args_namespace.word)
+
+    def test_no_arguments_passed(self):
+        args = []
+        self.assertRaises(SystemExit,
+                          self.cli_parser.parse_args,
+                          args)
+
+
+if __name__ == '__main__':
+    unittest.main()
